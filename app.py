83c09e70ce363d7dbb462abe7a748819f4b42da4
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-04-05 11:13:20 +0300

    Create new GUI

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+import tkinter as tk
 
 def processText(text):
     return re.sub(r'[^a-z]', ' ', text.lower())
@@ -62,33 +63,111 @@ def splitDataLabels():
 
 def getMatrixValues():
     x,y = splitDataLabels()
-    x = normalize(bagOfWord.fit_transform(x).toarray(),norm)
+    bagOfWord.fit(x)
+    x = normalize(bagOfWord.transform(x).toarray(),norm)
     return x,y
 
 def setSplitTestTrain():
-    x, y = getMatrixValues()
     global x_train,x_test,y_train,y_test
+    x, y = getMatrixValues()
     if not x_train:
         x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2)
+    
 
 """
     Prima data vom volosi un svm liniar sa vedem acuratetea
 """
 
+def getModel(type = 'SVM'):
+    model = None
+    if type == 'SVM':
+        model = SVC(kernel = 'linear')
+    else:
+        model  = KNeighborsClassifier(9)
+    return trainModel(model)
+
+def trainModel(model):
+    model.fit(x_train,y_train)
+    return model
+
 def testSvc():
     setSplitTestTrain()
-    model = SVC(kernel = 'linear')
-    model.fit(x_train,y_train)
+    model = getModel('SVC')
     predictions = model.predict(x_test)
     print(classification_report(y_test,predictions))
 
 def testKnn():
     setSplitTestTrain()
-    model = KNeighborsClassifier(9)
-    model.fit(x_train,y_train)
+    model = getModel('KNN')
     predictions = model.predict(x_test)
     print(classification_report(y_test,predictions))
 
 
+model = None
+root = tk.Tk()
+root.title('NLP project')
+modelSelections = None
+modelPredictions = None
+prediction = None
+textLabel = None
+buttonPredict = None
+predictions = ['Sad', 'Happy']
+
+def setVal():
+    text = prediction.get()
+    pred = predict(text)
+    textLabel.config(text=f'"{text}" -> {predictions[pred]}')
+    prediction.delete(0, tk.END)
+
+def generateModelSelection():
+    global modelSelections
+    modelSelections = tk.LabelFrame(root, text="Model selection",padx=5,pady=5)
+    modelSelections.pack()
+    tk.Button(modelSelections, text='KNN',command=setKNN).pack()
+    tk.Button(modelSelections, text='SVM',command=setSVM).pack()
+
+def generatePredictionSpace():
+    global modelPredictions, prediction, textLabel
+    modelPredictions = tk.LabelFrame(root, text='Model prediction',padx=5, pady=5)
+    modelPredictions.pack()
+    tk.Button(modelPredictions, text="predict",command=setVal).grid(row = 0, column = 0)
+    prediction = tk.Entry(modelPredictions, justify=tk.CENTER)
+    prediction.grid(row = 0, column = 1)
+    textLabel = tk.Label(modelPredictions,text="")
+    textLabel.grid(row= 1, column=0,columnspan=2)
+
+
+def destroyPredictionSpace():
+    modelPredictions.destroy()
+
+def setKNN():
+    global model
+    destroyPredictionSpace()
+    model = getModel('KNN')
+    generatePredictionSpace()
+
+
+
+def setSVM():
+    global model
+    destroyPredictionSpace()
+    model = getModel('SVM')
+    generatePredictionSpace()
+
+def main():
+    generateModelSelection()
+    generatePredictionSpace()
+    root.mainloop()
+
+setSplitTestTrain()
+model = getModel()
+
+def predict(string):
+    x = normalize(bagOfWord.transform([string]).toarray(),norm)
+    print(f'data -> {x}')
+    # y = model.predict(x[0])
+    return 1
+
+
 if __name__ == '__main__':
-    testKnn()
+    main()
